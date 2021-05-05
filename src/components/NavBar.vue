d550904e7311a506bd68e6177f3beb576a72aa99
Nano-Nano <nano2.aloerina@gmail.com>
2021-05-05 20:51:12 +0900

    アプリ名の表示設定

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="ui container">
     <div class="ui large secondary inverted pointing menu">
+      <div class="header item">vbc on Web</div>
       <router-link class="item" :class="{ active: props.page == 'Home' }" to="/">Home</router-link>
       <router-link class="item" :class="{ active: props.page == 'Simulate' }" to="/simulate">Simulate</router-link>
       <router-link class="item disabled" :class="{ active: props.page == 'Create' }" to="/">Create</router-link>
